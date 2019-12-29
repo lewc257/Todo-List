c80c0e5d8452e97dbd37b452955b9325e8a26a03
Lewis <djl700@live.co.uk>
2019-12-29 13:47:26 +0000

    first commit

---
+++ WebContent/constants.js
@@ -0,0 +1,3 @@
+//Where data storage is sent: https://stackoverflow.com/questions/8634058/where-the-sessionstorage-and-localstorage-stored
+const NO_STORAGE_ERROR_MSG = "No storage!";
+const MAX_CHARACTER_LENGTH = 52;
